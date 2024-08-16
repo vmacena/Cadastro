fbdcceeca4fe03b2b48948fd36470b104b0eee25
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-08-15 22:13:07 -0300

    feat: add top bar with title "Cadastro"

--- app/src/main/java/com/ifsp/cadastro/MainActivity.kt
+++ app/src/main/java/com/ifsp/cadastro/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.ifsp.cadastro
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
