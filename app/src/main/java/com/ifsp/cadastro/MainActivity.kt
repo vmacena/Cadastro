59123d7d441f14834808dd521cd67187f1b60db4
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-08-16 00:27:56 -0300

    feat: implement clear and save button logic

--- app/src/main/java/com/ifsp/cadastro/MainActivity.kt
+++ app/src/main/java/com/ifsp/cadastro/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ifsp.cadastro
 
 import android.os.Bundle
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -11,5 +12,65 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        val etNomeCompleto: EditText =
+            findViewById(R.id.etNomeCompleto)
+        val etTelefone: EditText =
+            findViewById(R.id.etTelefone)
+        val etEmail: EditText =
+            findViewById(R.id.etEmail)
+        val cbListaEmails: CheckBox =
+            findViewById(R.id.cbListaEmails)
+        val rgSexo: RadioGroup =
+            findViewById(R.id.rgSexo)
+        val etCidade: EditText =
+            findViewById(R.id.etCidade)
+        val spUf: Spinner =
+            findViewById(R.id.spUf)
+        val btnLimpar: Button =
+            findViewById(R.id.btnLimpar)
+        val btnSalvar: Button =
+            findViewById(R.id.btnSalvar)
+
+        val estados =
+            resources.getStringArray(R.array.estados_brasil)
+        val adapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, estados)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spUf.adapter = adapter
+
+        btnSalvar.setOnClickListener {
+            val nomeCompleto =
+                etNomeCompleto.text.toString()
+            val telefone =
+                etTelefone.text.toString()
+            val email =
+                etEmail.text.toString()
+            val ingressarListaEmails =
+                cbListaEmails.isChecked
+            val sexo =
+                when (rgSexo.checkedRadioButtonId) {
+                R.id.rbMasculino -> "Masculino"
+                R.id.rbFeminino -> "Feminino"
+                else -> ""
+            }
+            val cidade =
+                etCidade.text.toString()
+            val uf =
+                spUf.selectedItem.toString()
+
+            val formulario = Formulario(nomeCompleto, telefone, email, ingressarListaEmails, sexo, cidade, uf)
+            Toast.makeText(this, formulario.toString(), Toast.LENGTH_LONG).show()
+        }
+
+        btnLimpar.setOnClickListener {
+            etNomeCompleto.text.clear()
+            etTelefone.text.clear()
+            etEmail.text.clear()
+            cbListaEmails.isChecked = false
+            rgSexo.clearCheck()
+            etCidade.text.clear()
+            spUf.setSelection(0)
+        }
     }
 }
